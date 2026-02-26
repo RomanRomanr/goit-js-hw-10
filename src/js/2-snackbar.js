4375f007b128e06a2a484d577b1a36e599c19734
Roman <romario.higa@gmail.com>
2026-02-26 17:40:30 +0100

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,50 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delay = form.elements.delay;
+const state = form.elements.state;
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const delayValue = Number(delay.value);
+    let chooseState;
+    for (let i = 0; i < state.length; i++) {
+        if (state[i].checked) {
+            chooseState = state[i].value;
+            break;
+        }
+    }
+    if (!chooseState) {
+        iziToast.error({
+            message: 'Please select a state',
+            position: 'topRight',
+        });
+        return;
+    }
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (chooseState === 'fulfilled') {
+          resolve(delayValue); 
+        } else {
+          reject(delayValue); 
+        }
+      }, delayValue);
+    });
+
+    
+    promise
+      .then(delay => {
+        iziToast.success({
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          message: `Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+
+    form.reset();
+});
